76bd2ad63db7509e7e301953102da84ef3f5211b
Felix Klein <felix@qbaylogic.com>
2024-11-05 17:33:52 +0100

    Specification/implementation improvements (#6)
    Improves the specification and implementation via
    * using a sliding window for accessing past values of Wₜ
    * using a Mealy machine for iterating over the computation cycles instead of passing everything through a register chain

--- src/Clash/Signal/Delayed/Extra.hs
+++ src/Clash/Signal/Delayed/Extra.hs
@@ -15,20 +15,18 @@ module Clash.Signal.Delayed.Extra
 
 import Clash.Prelude
 
+import Data.Maybe (isJust)
 import Language.Haskell.Unicode (type (≤))
 
 -- | Temporally folds a signal over time. The folding function is
--- allowed to introduce an m-cycle delay and is assumed to require the
--- inputs to be stable for at least @m@ cycles. 'dsFold' takes care
--- about satisifying the stability requirements for the accumulator,
--- but stability of the input stream needs the asserted outside of
--- 'dsFold', simply for the reason of minimizing register usage, as
--- the latches for keeping the input streams stable also may be used
--- elsewhere.
+-- expected to take m-cycles and is assumed to only receive
+-- ('Just'-wrapped) inputs with a temporal distance of at least @m@
+-- cycles, i.e., there must be at least @m-1@ consecutive 'Nothing'
+-- inputs between every two consecutive 'Just's.
 --
--- A step trigger is used to start the next round of a fold, where
--- every two consequitve assertions the trigger must be at least @m@
--- cycles apart.
+-- A new round of a fold is initiated via providing a 'Just'-wrapped
+-- input, where every two consequitve 'Just'-wrapped inputs must be at
+-- least @m@ cycles apart.
 
 -- TODO: model the aformentioned assumptions as part of the type.
 dsFold ∷
@@ -37,22 +35,23 @@ dsFold ∷
   b →
   -- ^ initial value of the accumulator (only used after releasing the
   -- reset)
-  DSignal dom (k + m) Bool →
-  -- ^ step trigger
-  (DSignal dom k b → DSignal dom k a → DSignal dom (k + m) b) →
+  (DSignal dom k (Maybe (a, b)) → DSignal dom (k + m) b) →
   -- ^ function / circuit to be folded
-  DSignal dom k a →
-  -- ^ input stream
+  DSignal dom k (Maybe a) →
+  -- ^ input stream, where relevant inputs are 'Just'-wrapped
   DSignal dom (k + m) b
   -- ^ output stream
-dsFold ival trg circuit is = result
+dsFold ival circuit input = result
  where
-  result = dmux (circuit (antiDelay (SNat @m) acc) is) acc
+  result = case compareSNat @m @0 SNat SNat of
+    SNatLE → acc
+    SNatGT → mux (delayedI @m False (isJust <$> input))
+               ( circuit
+               $ (\i a → ( , a) <$> i)
+                   <$> input
+                   <*> antiDelay (SNat @m) acc
+               )
+               acc
 
   acc ∷ DSignal dom (k + m) b
   acc = delayedI @1 @b @dom @(k + m - 1) ival $ antiDelay d1 result
-
-  dmux ∷ DSignal dom (k + m) b → DSignal dom (k + m) b → DSignal dom (k + m) b
-  dmux = case compareSNat @m @0 SNat SNat of
-    SNatLE → const
-    SNatGT → mux trg
