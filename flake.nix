6a7e2b693db64e46b81bff920ec7a4d51bdd8388
Felix Klein <felix@qbaylogic.com>
2024-12-24 11:36:30 +0100

    Add shake support

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   outputs = { self, nixpkgs, flake-utils, clash-compiler, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = clash-compiler.inputs.nixpkgs.legacyPackages.${system};
+          inherit (pkgs.haskell.lib) dontCheck doJailbreak markUnbroken;
           overlay = final: prev: {
             clash-cores = clash-compiler.packages.${system}.clash-cores;
             clash-prelude = clash-compiler.packages.${system}.clash-prelude;
@@ -15,6 +16,7 @@
             clash-lib = clash-compiler.packages.${system}.clash-lib;
             clash-ghc = clash-compiler.packages.${system}.clash-ghc;
             cabal-install = nixpkgs.legacyPackages.${system}.cabal-install;
+            serialport = doJailbreak (prev.callHackage "serialport" "0.5.6" { });
           };
           # TODO: refer dynamically to the right ghc version (using clash-compiler's default).
           # Might require some changes on clash-compiler's flake.
