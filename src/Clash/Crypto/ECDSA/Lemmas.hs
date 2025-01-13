2e56d517afa3292826d6ac8164d0ff7b223cd3ed
Diego Diverio <diego@monads.fr>
2025-01-13 10:05:55 +0100

    Lint and improvements.
    Update lemmas.
    
    Some refactoring.
    
    Change from `DSignal` to counters.

--- src/Clash/Crypto/ECDSA/Lemmas.hs
+++ src/Clash/Crypto/ECDSA/Lemmas.hs
@@ -3,16 +3,15 @@
 module Clash.Crypto.ECDSA.Lemmas where
 import Clash.Prelude
 import Data.Constraint (Dict (..))
-import qualified GHC.TypeLits as P
 import Unsafe.Coerce (unsafeCoerce)
 
-lemma_mod :: forall len n m. (KnownNat len, KnownNat n, KnownNat m, n ~ m + 1, P.Mod len (2 ^ n) ~ 0) =>
- Dict (P.Mod (Div len 2) (2 ^ m) ~ 0)
+lemma_mod :: forall len n m. (n ~ m + 1, len `Mod` (2 ^ n) ~ 0) =>
+ Dict ((Div len 2) `Mod` (2 ^ m) ~ 0)
 lemma_mod = unsafeCoerce (Dict :: Dict (0 ~ 0))
 
-lemma_mul_div :: forall len n. (KnownNat len, KnownNat n, 1 <= n) =>
+lemma_mul_div :: forall len n. (1 <= n, len `Mod` n ~ 0) =>
  Dict (Div len n * n ~ len)
 lemma_mul_div = unsafeCoerce (Dict :: Dict (0 ~ 0))
 
-lemma_pow :: forall n. (KnownNat n) => Dict (1 <= 3 ^ n)
+lemma_pow :: forall n. Dict (1 <= 3 ^ n)
 lemma_pow = unsafeCoerce (Dict :: Dict (0 <= 0))
