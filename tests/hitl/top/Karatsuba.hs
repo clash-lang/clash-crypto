272c5e14f9c293e409978e0074e2ba40364eb801
Diego Diverio <diego@monads.fr>
2025-04-18 11:27:30 +0200

    Remove commented code

--- tests/hitl/top/Karatsuba.hs
+++ tests/hitl/top/Karatsuba.hs
@@ -79,38 +79,4 @@ top rx = tx
   v << n
     = fst $ shiftOutFrom0 n v
 
--- | We use `0x00` as an escape symbol to mark the end of the input
---
--- > 0x00 0b00000000   denotes `0x00` (no end marker)
--- > 0x00 0b???????1   marks the last byte with 7 bits used
--- > 0x00 0b??????10   marks the last byte with 6 bits used
--- > 0x00 0b?????100   marks the last byte with 5 bits used
--- > 0x00 0b????1000   marks the last byte with 4 bits used
--- > 0x00 0b???10000   marks the last byte with 3 bits used
--- > 0x00 0b??100000   marks the last byte with 2 bits used
--- > 0x00 0b?1000000   marks the last byte with 1 bits used
--- > 0x00 0b10000000   marks the previous byte to be the last byte
--- descape ∷
---   HiddenClockResetEnable dom ⇒
---   Signal dom (Maybe (BitVector 8)) →
---   Signal dom (Maybe (BitVector 8, Maybe (Index 9)))
--- descape = mealy (~~>) False
---  where
---   esc   ~~> Nothing   = (esc, Nothing)
---   False ~~> Just 0x00 = (True, Nothing)
---   False ~~> Just byte = (False, Just (byte, Nothing))
---   True  ~~> Just 0x00 = (False, Just (0x00, Nothing))
---   True  ~~> Just byte = (False, Just (byte, Just $ trailingZeros + 1))
---    where
---     trailingZeros
---       | testBit byte 0 = 0
---       | testBit byte 1 = 1
---       | testBit byte 2 = 2
---       | testBit byte 3 = 3
---       | testBit byte 4 = 4
---       | testBit byte 5 = 5
---       | testBit byte 6 = 6
---       | testBit byte 7 = 7
---       | otherwise      = 8
-
 makeTopEntity 'topEntity
