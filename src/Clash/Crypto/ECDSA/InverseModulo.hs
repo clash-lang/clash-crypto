1739f91603baf8b4c81522c7e53e102f2ff63f4b
Diego Diverio <diego@monads.fr>
2025-04-18 13:50:23 +0200

    Add inverse modulo and simulation tests

---
+++ src/Clash/Crypto/ECDSA/InverseModulo.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE AllowAmbiguousTypes, UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module Clash.Crypto.ECDSA.InverseModulo where
+
+import Clash.Prelude hiding (Mod)
+import Clash.Crypto.ECDSA.Modulo (ModSize, Mod (..), unMod)
+import Clash.Crypto.ECDSA.Utils (signedToUnsigned)
+import Data.Maybe (isJust, fromJust)
+
+-- * Working implementations
+
+-- |A streaming implementation of the Binary Euclidean Algorithm.
+-- It computes an inverse modulo m.
+-- `forall n. (bea @m n * n) `mod` (natToNum @m) == 1`
+bea :: forall m dom. (KnownNat m, KnownDomain dom, HiddenClockResetEnable dom, 1 <= m) =>
+ Signal dom (Maybe (Mod m)) ->
+ Signal dom (Maybe (Mod m))
+bea s = fmap (Mod @m) <$> mealy (~~>) BeaIdle s
+ where
+  (~~>) :: BeaState m -> Maybe (Mod m) -> (BeaState m, Maybe (Unsigned (ModSize m)))
+  _ ~~> Just a = (BeaStart (bitCoerce $ resize $ unMod a, natToNum @m, 1, 0), Nothing)
+  BeaIdle ~~> Nothing = (BeaIdle, Nothing)
+  BeaStart (u, v, x, y) ~~> Nothing =
+   let state = if u /= 1 && v /= 1 then BeaUMod2 (u, v, x, y) else BeaEnd (u, v, x, y)
+   in (state, Nothing)
+  BeaUMod2 (u, v, x, y) ~~> Nothing =
+   if lsb u == low then
+    let u' = u `shiftR` 1
+        x' = if lsb x == low then x `shiftR` 1 else (x + natToNum @m) `shiftR` 1
+    in (BeaUMod2 (u', v, resize x', y), Nothing)
+   else (BeaVMod2 (u, v, x, y), Nothing)
+  BeaVMod2 (u, v, x, y) ~~> Nothing =
+   if lsb v == low then
+    let v' = v `shiftR` 1
+        y' = if lsb y == low then y `shiftR` 1 else (y + natToNum @m) `shiftR` 1
+    in (BeaVMod2 (u, v', x, resize y'), Nothing)
+   else (BeaCompare (u, v, x, y), Nothing)
+  BeaCompare (u, v, x, y) ~~> Nothing =
+   if u >= v then
+    let u' = (u - v)
+        x' = (x - y)
+    in (BeaModU (resize u', v, resize x', y), Nothing)
+   else
+    let v' = (v - u)
+        y' = (y - x)
+    in (BeaModV (u, resize v', x, resize y'), Nothing)
+  BeaModU (u, v, x, y) ~~> Nothing =
+   let res = specialMod u in
+   if isJust res then (BeaModU (fromJust res, v, x, y), Nothing) else (BeaModX (u, v, x, y), Nothing)
+  BeaModX (u, v, x, y) ~~> Nothing =
+   let res = specialMod x in
+   if isJust res then (BeaModX (u, v, fromJust res, y), Nothing) else (BeaStart (u, v, x, y), Nothing)
+  BeaModV (u, v, x, y) ~~> Nothing =
+   let res = specialMod v in
+   if isJust res then (BeaModV (u, fromJust res, x, y), Nothing) else (BeaModY (u, v, x, y), Nothing)
+  BeaModY (u, v, x, y) ~~> Nothing =
+   let res = specialMod y in
+   if isJust res then (BeaModY (u, v, x, fromJust res), Nothing) else (BeaStart (u, v, x, y), Nothing)
+  BeaEnd (u, _, x, y) ~~> Nothing =
+   if u == 1
+    then (BeaIdle, Just $ signedToUnsigned $ resize x)
+    else (BeaIdle, Just $ signedToUnsigned $ resize y)
+  specialMod val =
+   if val <= natToNum @m then
+    if val < 0 then Just $ val + natToNum @m else Nothing
+   else Just $ val - natToNum @m
+
+data BeaState (m :: Nat) =
+ BeaIdle
+ | BeaStart (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaUMod2 (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaVMod2 (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaCompare (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaModU (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaModV (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaModX (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaModY (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ | BeaEnd (Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2), Signed (ModSize m * 2))
+ 
+ deriving (Generic, NFDataX, Show)
---
+++ tests/sim/Test/Clash/Crypto/ECDSA/InverseModulo.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+module Test.Clash.Crypto.ECDSA.InverseModulo where
+import Test.Tasty
+import Test.Tasty.Hedgehog
+import Clash.Prelude hiding (Mod)
+import Hedgehog
+import Hedgehog.Gen as Gen
+import Hedgehog.Range as Range
+import qualified Data.List as List
+import Data.Maybe (catMaybes)
+import Clash.Crypto.ECDSA.InverseModulo (bea)
+import Clash.Crypto.ECDSA.Modulo
+
+-- TODO: Once all PRs are merged, move this to one place.
+type Q = 115792089210356248762697446949407573530086143415290314195533631308867097853951
+
+type Modulo = Q
+
+tastyTests :: TestTree
+tastyTests = testGroup "InverseModulo"
+  [ localOption (HedgehogTestLimit (Just 1000)) $ testInverse]
+
+testInverse = testProperty "functional equality" myProp
+
+-- Note: Modulus should always be prime for this algorithm to work.
+type MyMod = Q
+
+-- Given a modulus g, forall 0 < f < g, show `bea f` returns the inverse of f in g.
+myProp :: Property
+myProp = property $ do
+  f <- forAll (generator (natToNum @MyMod))
+  let f' = fromIntegral $ calcBea @MyMod (fromInteger f)
+  (f' * f) `mod` (natToNum @MyMod) === 1
+ where
+  generator mod = Gen.integral (Range.constantFrom (1) 1 (mod-1))
+
+  calcBea ::
+    forall m.
+    (KnownNat m, 1 <= m) =>
+    Mod m ->
+    Mod m
+  calcBea input =
+    List.head
+     $ catMaybes
+     $ sampleN @System 4000
+     $ withClockResetEnable clockGen resetGen enableGen
+     $ bea @m (beaInput input)
+
+  beaInput input =
+    fromList
+      $ [Nothing, Nothing]
+        <> [Just input]
+        <> List.repeat Nothing
