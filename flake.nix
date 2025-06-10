c44c35e7f936d6113f05c22117bde8f0186d81d0
Diego Diverio <diego@monads.fr>
2025-06-10 20:08:46 +0200

    Remove clash-cores

--- flake.nix
+++ flake.nix
@@ -14,12 +14,6 @@
             rev = "ce42a5afebb55d2e2e84be7f5386d69c343e3942";
             sha256 = "sha256-oBG8DylFwzUu212AiNOg2x+D1jmI2hAvMJQT0F8wWlE=";
           };
-          clashCoresSrc = pkgs.fetchFromGitHub {
-            owner = "clash-lang";
-            repo = "clash-cores";
-            rev = "6b9f2b76ccdaf58f9a4b35566e13c30d3976f037";
-            sha256 = "sha256-8MDLd1bpc7qLFUTBiw92v2scDuD3z7EGVh52PPfG1Ak=";
-          };
           clashProtocolsSrc = pkgs.fetchFromGitHub {
             owner = "clash-lang";
             repo = "clash-protocols";
@@ -32,16 +26,9 @@
             rev = "462de2c8ae81eb525ad0df04a69ac73384baa309";
             sha256 = "sha256-P/7iDg+35Bz4KwyixcAgPpmx71TfpommpuvOiyZ3T9Q=";
           };
-          circuitNotationSrc = pkgs.fetchFromGitHub {
-            owner = "cchalmers";
-            repo = "circuit-notation";
-            rev = "564769c52aa05b90f81bbc898b7af7087d96613d";
-            sha256 = "sha256-sPfLRjuMxqVRMzXrHRCuKKrdTdqgAJ33pf11DoTP84Q=";
-          };
 
           inherit (pkgs.haskell.lib) dontCheck doJailbreak markUnbroken;
           overlay = final: prev: {
-            clash-cores = prev.callCabal2nix "clash-cores" clashCoresSrc { };
             clash-protocols = doJailbreak (prev.callCabal2nix "clash-protocols"
               (clashProtocolsSrc + "/clash-protocols") { });
             clash-protocols-base = prev.callCabal2nix "clash-protocols-base"
@@ -56,7 +43,6 @@
               (clashCompilerSrc + "/clash-ghc") { };
             # Otherwise fails on `template-haskell < 2.22`.
             serialport = dontCheck (prev.callCabal2nix "serialport" serialportSrc { });
-            circuit-notation = prev.callCabal2nix "circuit-notation" circuitNotationSrc { };
             clash-crypto = final.callCabal2nix "clash-crypto" ./. { };
           };
           myHsPkgs = pkgs.haskell.packages.ghc910.extend overlay;
