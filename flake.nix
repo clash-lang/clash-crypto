ed70c32419abc27393f268655128f6aff7ebaa90
Diego <119593839+diegodiv@users.noreply.github.com>
2025-06-16 14:27:16 +0200

    Update flake (#21)
    Update the flake to depend on the newest version of `clash-compiler` and remove unneeded dependencies.

--- flake.nix
+++ flake.nix
@@ -3,45 +3,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    clash-compiler.url = "github:clash-lang/clash-compiler/dd/nix-update";
     ecpprog.url = "github:diegodiv/ecpprog";
   };
-  outputs = { self, nixpkgs, flake-utils, clash-compiler, ecpprog, ... }:
+  outputs = { self, nixpkgs, flake-utils, ecpprog, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = clash-compiler.inputs.nixpkgs.legacyPackages.${system};
+      let pkgs = nixpkgs.legacyPackages.${system};
           serialportSrc = pkgs.fetchFromGitHub {
             owner = "standardsemiconductor";
             repo = "serialport";
             rev = "ce42a5afebb55d2e2e84be7f5386d69c343e3942";
             sha256 = "sha256-oBG8DylFwzUu212AiNOg2x+D1jmI2hAvMJQT0F8wWlE=";
           };
+          clashCompilerSrc = pkgs.fetchFromGitHub {
+            owner = "clash-lang";
+            repo = "clash-compiler";
+            rev = "7bf85bfbdb6561c068f99ec5f346d1b5092a011b";
+            sha256 = "sha256-uiXDG8S5eJrQvwetU0YlAKu1C5RDFoW/3/j77nM0lYw=";
+          };
+
           inherit (pkgs.haskell.lib) dontCheck doJailbreak markUnbroken;
           overlay = final: prev: {
-            clash-prelude = clash-compiler.packages.${system}.clash-prelude;
-            clash-prelude-hedgehog = clash-compiler.packages.${system}.clash-prelude-hedgehog;
-            clash-lib = clash-compiler.packages.${system}.clash-lib;
-            clash-ghc = clash-compiler.packages.${system}.clash-ghc;
-            cabal-install = nixpkgs.legacyPackages.${system}.cabal-install;
-            serialport = dontCheck (doJailbreak (prev.callCabal2nix "serialport" serialportSrc { }));
+            clash-prelude = prev.callCabal2nix "clash-prelude"
+              (clashCompilerSrc + "/clash-prelude") { };
+            clash-prelude-hedgehog = prev.callCabal2nix "clash-prelude-hedgehog"
+              (clashCompilerSrc + "/clash-prelude-hedgehog") { };
+            clash-lib = prev.callCabal2nix "clash-lib"
+              (clashCompilerSrc + "/clash-lib") { };
+            clash-ghc = prev.callCabal2nix "clash-ghc"
+              (clashCompilerSrc + "/clash-ghc") { };
             # Otherwise fails on `template-haskell < 2.22`.
-            string-interpolate = doJailbreak (prev.string-interpolate);
-            typelits-witnesses = markUnbroken prev.typelits-witnesses;
+            serialport = dontCheck (prev.callCabal2nix "serialport" serialportSrc { });
+            clash-crypto = final.callCabal2nix "clash-crypto" ./. { };
           };
-          # TODO: refer dynamically to the right ghc version (using clash-compiler's default).
-          # Might require some changes on clash-compiler's flake.
-          myHsPkgs = (pkgs.haskell.packages.ghc910.extend overlay).extend
-            (pkgs.haskell.lib.compose.packageSourceOverrides {
-              clash-crypto = ./.;
-            });
+          myHsPkgs = pkgs.haskell.packages.ghc910.extend overlay;
       in
       {
         devShells.default = myHsPkgs.shellFor {
           name = "ghc910";
           packages = p: [ p.clash-crypto ];
-          inputsFrom = [
-            clash-compiler.packages.${system}.clash-lib.env
-            clash-compiler.packages.${system}.clash-ghc.env
-          ];
+          inputsFrom = [];
           nativeBuildInputs =
             with pkgs; [
               gnumake yosys nextpnr trellis
@@ -54,6 +54,6 @@
             export PATH="$(dirname $SHAKEPATH):$PATH:$(dirname $SHAKEPATH)"
           '';
         };
-        default = myHsPkgs.clash-crypto;
+        packages.default = dontCheck myHsPkgs.clash-crypto;
       });
 }
