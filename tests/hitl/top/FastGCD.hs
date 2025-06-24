bee9bd8a94ece6e4f11db15a26019ab3e130f0ab
Diego Diverio <diego@monads.fr>
2025-06-24 10:36:25 +0200

    Update FastGCD HITL test

--- tests/hitl/top/FastGCD.hs
+++ tests/hitl/top/FastGCD.hs
@@ -2,18 +2,19 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE CPP #-}
 
-module FastGCD where
+module FastGCD (topEntity) where
 
-import Clash.Prelude hiding (Mod)
-
+import Clash.Prelude
 import Clash.Annotations.TH (makeTopEntity)
-import Clash.Cores.UART (uart)
 
-import Domain (Dom48, Dom12)
-import Pll (orangePll12)
-import Clash.Crypto.ECDSA.Modulo (ModSize)
+import Clash.Cores.LatticeSemi.ECP5.Domain (Dom48, Dom12)
+import Clash.Cores.LatticeSemi.ECP5.Pll (orangePll12)
+import Clash.Crypto.Hitlt.Shared (Q)
+import Clash.Crypto.Hitlt.Uart (bulkRead, withUartRequestResponseHandler)
+
 import Clash.Crypto.ECDSA.InverseModulo (fastGcdSequential)
-import Data.Maybe (isJust, fromMaybe)
+
+import Data.Maybe (isJust)
 
 -- allows to select the UART baud via a CPP define
 #ifndef HITLT_BAUD
@@ -22,61 +23,18 @@ type BAUD = 9600
 type BAUD = HITLT_BAUD
 #endif
 
--- TODO: Once all PRs are merged, move this to one place.
-type Q = 115792089210356248762697446949407573530086143415290314195533631308867097853951
-type IntegerSize = ModSize Q
-
 topEntity ∷
   "CLK" ::: Clock Dom48 →
   "PMOD1_6" ::: Signal Dom12 Bit →
   "PMOD1_5" ::: Signal Dom12 Bit
-topEntity (orangePll12 → (clk12, rst12)) =
-  withClockResetEnable clk12 rst12 enableGen top
-
-top ∷
-  HiddenClockResetEnable Dom12 ⇒
-  Signal Dom12 Bit → Signal Dom12 Bit
-top rx = tx
- where
-  (rxData, tx, ack) = uart (SNat @BAUD) rx txReq
-
-  toggleSwitch = isJust <$> dataLine
-  toggle = register False $ mux toggleSwitch (not <$> toggle) toggle
-  dataLine = fmap bitCoerce <$> mealy bufferStep (0, def) rxData
-  dataReg = register 0 $ mux (isJust <$> dataLine) (fromMaybe 0 <$> dataLine) dataReg
-  result = register Nothing $ fmap bitCoerce <$> fastGcdSequential @Q @Dom12
-    toggle dataReg
-
-  bufferStep ::
-    (Index (IntegerSize `Div` 8), Vec (IntegerSize `Div` 8) (BitVector 8)) ->
-    Maybe (BitVector 8) ->
-    ((Index (IntegerSize `Div` 8), Vec (IntegerSize `Div` 8) (BitVector 8)),
-     Maybe (Vec (IntegerSize `Div` 8) (BitVector 8)))
-  bufferStep state Nothing = (state, Nothing)
-  bufferStep (i, v) (Just val) =
-    if i == maxBound then ((0,def), Just nv)
-    else ((i + 1, nv), Nothing)
-    where nv = v <<+ val
-
-  txReq = mealyB (~~>)
-    (def, 0 ∷ Index (IntegerSize `Div` 8 + 1))
-    (fmap toVec8 <$> result, ack)
-
-  -- wait for the response from the crypto core
-  s@(_, 0) ~~> (Nothing, _) = (s, Nothing)
-  -- response received, start sending bytes
-  (_, 0)   ~~> (Just h,  _) = ((h, maxBound), Just $ head h)
-  -- wait for the UART ack of the byte previously sent
-  s@(h, _) ~~> (_,   False) = (s, Just $ head h)
-  -- UART ack received
-  (h, n)   ~~> (_,    True) = ((h << d1, n - 1), Just $ head h)
-
-  toVec8 ∷
-    Unsigned IntegerSize →
-    Vec (IntegerSize `Div` 8) (BitVector 8)
-  toVec8 = bitCoerce
-
-  v << n
-    = fst $ shiftOutFrom0 n v
+topEntity (orangePll12 → (clk, rst))
+  = withUartRequestResponseHandler clk rst (SNat @BAUD)
+  $ \(bulkRead → request) →
+      let
+        -- switch the toggle when a new value is received
+        toggle = register False $ toggle ./=. (isJust <$> request)
+        x = regMaybe 0 request
+      in
+        fastGcdSequential @Q toggle x
 
 makeTopEntity 'topEntity
