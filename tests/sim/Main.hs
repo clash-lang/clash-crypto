b45b07a6777f8de6ff0cd8228380341e74cc965d
monecky <marijkevaniperen@gmail.com>
2025-09-24 10:29:25 +0200

    stable debug

--- tests/sim/Main.hs
+++ tests/sim/Main.hs
@@ -2,11 +2,11 @@ import Prelude
 import Test.Tasty
 
 import qualified Test.Clash.Crypto.Cipher.AES as AES
-import qualified Test.Clash.Crypto.Hash.SHA as SHA
-import qualified Test.Clash.Crypto.MAC.HMAC as HMAC
-import qualified Test.Clash.Crypto.ECDSA.Karatsuba as Karatsuba
-import qualified Test.Clash.Crypto.ECDSA.Modulo as Modulo
-import qualified Test.Clash.Crypto.ECDSA.InverseModulo as InverseModulo
+-- import qualified Test.Clash.Crypto.Hash.SHA as SHA
+-- import qualified Test.Clash.Crypto.MAC.HMAC as HMAC
+-- import qualified Test.Clash.Crypto.ECDSA.Karatsuba as Karatsuba
+-- import qualified Test.Clash.Crypto.ECDSA.Modulo as Modulo
+-- import qualified Test.Clash.Crypto.ECDSA.InverseModulo as InverseModulo
 
 main ∷ IO ()
 main = defaultMain $ testGroup "clash-crypto simulation tests"
