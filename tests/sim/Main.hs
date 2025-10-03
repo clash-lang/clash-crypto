aef3d07f3a756aed26dc3292c11c08580fce5228
monecky <marijkevaniperen@gmail.com>
2025-10-03 11:46:45 +0200

    Adding the tests of the other crypto algorithms back

--- tests/sim/Main.hs
+++ tests/sim/Main.hs
@@ -2,18 +2,18 @@ import Prelude
 import Test.Tasty
 
 import qualified Test.Clash.Crypto.Cipher.AES as AES
--- import qualified Test.Clash.Crypto.Hash.SHA as SHA
--- import qualified Test.Clash.Crypto.MAC.HMAC as HMAC
--- import qualified Test.Clash.Crypto.ECDSA.Karatsuba as Karatsuba
--- import qualified Test.Clash.Crypto.ECDSA.Modulo as Modulo
--- import qualified Test.Clash.Crypto.ECDSA.InverseModulo as InverseModulo
+import qualified Test.Clash.Crypto.Hash.SHA as SHA
+import qualified Test.Clash.Crypto.MAC.HMAC as HMAC
+import qualified Test.Clash.Crypto.ECDSA.Karatsuba as Karatsuba
+import qualified Test.Clash.Crypto.ECDSA.Modulo as Modulo
+import qualified Test.Clash.Crypto.ECDSA.InverseModulo as InverseModulo
 
 main ∷ IO ()
 main = defaultMain $ testGroup "clash-crypto simulation tests"
   [ AES.tastyTests
-  -- , SHA.tastyTests
-  -- , HMAC.tastyTests
-  -- , InverseModulo.tastyTests
-  -- , Karatsuba.tastyTests
-  -- , Modulo.tastyTests
+  , SHA.tastyTests
+  , HMAC.tastyTests
+  , InverseModulo.tastyTests
+  , Karatsuba.tastyTests
+  , Modulo.tastyTests
   ]
