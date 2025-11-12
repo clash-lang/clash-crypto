c0591d90e562626d1ae327760f1ac914864031b6
Diego Diverio <diego@monads.fr>
2025-11-12 15:53:59 +0100

    Some changes for common lemmas

--- RocqProofs/Common.v
+++ RocqProofs/Common.v
@@ -14,42 +14,29 @@ Qed.
 
 (* Taken from a more recent version of mathcomp *)
 Lemma ltn_mull m1 m2 n1 n2 : 0 < n2 -> m1 < n1 -> m2 <= n2 -> m1 * m2 < n1 * n2.
-  Proof.
-    move=> n20 lt_mn1 le_mn2.
-    rewrite (@leq_ltn_trans (m1 * n2)) ?leq_mul2l ?le_mn2 ?orbT//.
-    by rewrite ltn_mul2r lt_mn1 n20.
+Proof.
+  move=> n20 lt_mn1 le_mn2.
+  rewrite (@leq_ltn_trans (m1 * n2)) ?leq_mul2l ?le_mn2 ?orbT//.
+  by rewrite ltn_mul2r lt_mn1 n20.
 Qed.
 
 Lemma toNat_shlB_zExtend : forall (bitlength extra : nat) (x : BITS bitlength) (shift : nat),
     1 <= bitlength -> shift <= extra ->
     toNat (shlBn (zeroExtend extra x) shift) = toNat x * 2 ^ shift.
-  move=> bitlength extra x shift Hbl.
-  elim: shift => [|shift IHshift] leq_shift /=.
-  - by rewrite expn0 muln1 toNat_zeroExtend.
-  - rewrite shlB_asMul toNat_mulB toNat_fromNat.
-    have Hextra: 1 <= extra by rewrite -(ltn_predK leq_shift).
-    have Hblextra : 2 <= bitlength + extra by rewrite -addn1; apply (leq_add Hbl Hextra).
-    have Hpower : 4 <= 2 ^ (bitlength + extra).
-    rewrite [_ < _]/(_.+1 <= _) [4]/(2^2); apply leq_pexp2l.
-    + by [].
-    + exact Hblextra.
-      rewrite (IHshift (ltnW leq_shift)).
-      rewrite !div.modn_small.
-    + by rewrite -mulnA -expnSr.
-    + have H2lt3: 2 < 3 by [].
-      exact (ltn_trans H2lt3 Hpower).
-    + rewrite -mulnA -expnSr.
-      rewrite expnD.
-      apply ltn_mull.
-      * rewrite -{1}(exp0n (n := extra) Hextra).
-        apply ltn_exp2r. exact Hextra.
-      * by apply toNatBounded.
-      * apply leq_pexp2l. by []. exact leq_shift.
-      * apply (ltn_trans) with (n := 3). by []. exact Hpower.
+  move=> bitlength extra ? shift ?.
+  elim: shift => [|? IHshift] leq_shift /= ; first by rewrite expn0 muln1 toNat_zeroExtend.
+  rewrite shlB_asMul toNat_mulB toNat_fromNat.
+  have Hpower : 4 <= 2 ^ (bitlength + extra) by
+    rewrite [_ < _]/(_.+1 <= _) [4]/(2^2); apply leq_pexp2l ; first by [] ; last lia.
+  rewrite (IHshift (ltnW leq_shift)) !div.modn_small ; first 1 [ by rewrite -mulnA -expnSr | lia ]
+  ; last by apply ltn_trans with (n := 3) ; first by [] ; last lia.
+  rewrite -mulnA -expnSr expnD.
+  apply ltn_mull ; first 1 [ lia | by apply toNatBounded ].
+  by apply leq_pexp2l ; by [] ; exact leq_shift.
 Qed.
 
 Lemma zeroExtend_tcast :  forall n m z (bs: BITS n) (H: n = m), zeroExtend z (tcast H bs) = tcast (f_equal (fun x => x + z) H) (zeroExtend z bs).
-  move=> n m z bs H.
+  move=> ? m ?? H.
   by case: m / H.
 Qed.
 
@@ -58,35 +45,35 @@ Lemma add_comm2 : forall n m z, n + z = m + z -> z + n = z + m.
 Qed.
 
 Lemma contra_equiv : forall a b, ~~ a <-> ~~ b -> a <-> b.
-  move=> a b H.
+  move=> ?? H.
   by split ; apply contraLR ; apply H.
 Qed.
 
 
 Lemma msb_tcast : forall n m (bs: BITS n)(H: n = m), msb (tcast H bs) = msb bs.
-  move=> n m bs H.
+  move=> ? m ? H.
   by case: m / H.
 Qed.
 
 Lemma shlBn_tcast : forall n m s (bs: BITS n)(H: n = m), shlBn (tcast H bs) s = tcast H (shlBn bs s).
-  move=> n m s bs H.
+  move=> ? m ?? H.
   by case: m / H.
 Qed.
 
 Lemma zeroExtend_tcast_eq : forall n m z (bs: BITS n) (H : n + z = m),
     shlBn (tuple.tcast H (zeroExtend z bs)) z = tuple.tcast H (shlBn (zeroExtend z bs) z).
-  move=> n m z bs H.
+  move=> ? m ?? H.
   by case: m / H.
 Qed.
 
-Lemma zeroExtend_tcast_less : forall n m z s (bs: BITS n) (H : n + z = m), s <= z ->
-    shlBn (tuple.tcast H (zeroExtend z bs)) s = tuple.tcast H (shlBn (zeroExtend z bs) s).
-  move=> n m z s bs H H2.
+Lemma zeroExtend_tcast_less : forall n m z s (bs: BITS n) (H : n + z = m),
+                                                                      shlBn (tuple.tcast H (zeroExtend z bs)) s = tuple.tcast H (shlBn (zeroExtend z bs) s).
+  move=> ? m ??? H.
   by case: m / H.
 Qed.
 
 Lemma leq_mulL : forall a b c d, 0 < c -> b <= d -> a <= b * c -> a <= d * c.
-  move=> a b c d H0 H1 H2.
+  move=> ? b c ??? H2.
   apply (leq_trans (n := b * c)). apply H2.
   by rewrite leq_pmul2r //.
 Qed.
