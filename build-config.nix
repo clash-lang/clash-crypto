539f21d226c6a89f4d21fea776cca5cab3d71386
Pieter Bos <pieter@qbaylogic.com>
2026-01-27 11:24:45 +0100

    require build-config-local

--- build-config.nix
+++ build-config.nix
@@ -1,13 +1,9 @@
-# To alter build configuration settings, please create and edit
-# build-config-local.nix, but be careful not to commit changes. The better
-# solution is to have local settings in a file that is ignored via .gitignore,
-# but there currently is no good solution to address a flake that depends on
-# files that are not in the git index.
+# To alter build configuration settings, please edit build-config-local.nix, but
+# be careful not to commit changes. The better solution is to have local
+# settings in a file that is ignored via .gitignore, but there currently is no
+# good solution to address a flake that depends on files that are not in the git
+# index.
 {
   serial-speed = "115200";
   hitlt-serial-dev = "/dev/ttyUSB0";
-} // (
-  if builtins.pathExists ./build-config-local.nix
-  then import ./build-config-local.nix
-  else {}
-)
+} // import ./build-config-local.nix
